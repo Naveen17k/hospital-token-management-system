4b5e33db20e521fdab4bc012e97cc25a51b903b0
Naveen17k <vvnnkk2600@gmail.com>
2024-08-06 21:55:20 +0530

    Bug fixes

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "@/styles/globals.css";
-import { RegistrationProvider } from "./registration-toggle-context";
-export default function App({ Component, pageProps }) {
-  return (
-    <RegistrationProvider>
-      <Component {...pageProps} />
-    </RegistrationProvider>
-  );
-}
--- pages/socket-instance.js
+++
@@ -1,7 +0,0 @@
-import socketIOClient from "socket.io-client";
-import react from "react";
-const socket = socketIOClient("http://localhost:8080", {
-  transports: ["websocket"],
-});
-
-export default socket;
